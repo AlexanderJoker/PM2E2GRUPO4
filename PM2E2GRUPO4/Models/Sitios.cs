ddaea87edd53d2117f6a06d1daf46c07373363cb
liliana2327 <23hernandezlili23@gmail.com>
2022-07-17 15:46:59 -0600

    --------

--- PM2E2GRUPO4/Models/Sitios.cs
+++ PM2E2GRUPO4/Models/Sitios.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 
-public class Sitios
+namespace PM2E2GRUPO4.Models
 {
+
+public class Sitios
+   {
 		public int Id { get; set; }
         public string Descripcion { get; set; }
         public double Latitud { get; set; }
         public double Longitud { get; set; }
         public string Foto { get; set; }
         public string Audio { get; set; }
-	
+    }
 }
