c176d1c8675c1c0238d042301e7b8e482cd61d81
liliana2327 <23hernandezlili23@gmail.com>
2022-07-16 21:37:30 -0600

    Clase sitios

--- PM2E2GRUPO4.Android/MainActivity.cs
+++ PM2E2GRUPO4.Android/MainActivity.cs
@@ -21,7 +21,7 @@ namespace PM2E2GRUPO4.Droid
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
+       
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
---
+++ PM2E2GRUPO4/Models/Sitios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Sitios
+{
+	public class Sitios()
+	{
+		 public string Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Latitud { get; set; }
+        public string Longitud { get; set; }
+        public string Foto { get; set; }
+        public string Audio { get; set; }
+	}
+}
