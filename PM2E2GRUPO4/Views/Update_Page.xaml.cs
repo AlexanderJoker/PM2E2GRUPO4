cc592d32515809284348d1e051a13337d6499427
tJKkeZQoZlcssuXjVjNerQ <jossethale32@hotmail.com>
2022-07-18 00:56:17 -0600

    josseth

---
+++ PM2E2GRUPO4/Views/Update_Page.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2E2GRUPO4.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Update_Page : ContentPage
+    {
+        public Update_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            //metodo de tap de la imagen
+        }
+
+        private void Grabar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDetener_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
